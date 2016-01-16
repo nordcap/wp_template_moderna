3803fe45fb1a34d72c963c51ccab21d8f191a150
Alex Budaev <budaevaa@mail.ru>
2016-01-16 10:34:01 +0700

    На странице "Контакты" вставлена карта Москвы

--- contact.php
+++ contact.php
@@ -20,8 +20,8 @@ get_header(); ?>
     </section>
     <section id="content">
         <div class="map">
-            <iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Kuningan,+Jakarta+Capital+Region,+Indonesia&amp;aq=3&amp;oq=kuningan+&amp;sll=37.0625,-95.677068&amp;sspn=37.410045,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Kuningan&amp;t=m&amp;z=14&amp;ll=-6.238824,106.830177&amp;output=embed">
-            </iframe>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8724.570335362972!2d37.61551138463631!3d55.757548081461124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afc73d4b0c9%3A0x3d44d6cc5757cf4c!2z0JzQvtGB0LrQstCw!5e0!3m2!1sru!2sru!4v1452915085900" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
 
         <div class="container">
