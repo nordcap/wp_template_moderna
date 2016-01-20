027787291588d467bfabb374970aa41f7de92360
Alex Budaev <budaevaa@mail.ru>
2016-01-20 13:53:50 +0700

    Перевод шаблона Moderna на Wordpress закончен. Что было исключено из разработки. 1) Дизайн блока комментариев (т.к. нет исходного дизайна)

--- template-parts/headline.php
+++ template-parts/headline.php
@@ -4,8 +4,11 @@
             <div class="col-lg-12">
                 <ul class="breadcrumb">
                     <li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
-                    <li class="active"><?php esc_html_e(the_title());?></li>
-<!--                    TODO: хлебные крошки должны быть как в исходнике-->
+                    <li class="active">
+                        <?php esc_html_e(the_title());
+                        /** Обображение немного не соответствует исх.шаблону, но считаю это не слишком принципиально. Т.к. заголовок подменю не является страницей а пустой ссылкой */?>
+                    </li>
+
                 </ul>
             </div>
         </div>
